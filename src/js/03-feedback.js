ebb61cd5a6da419f8063f2906debd25850ee350e
Andrii Marchuk <andrii.marchuck@gmail.com>
2022-12-25 16:20:08 +0200

    add Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(saveInput, 500));
+
+form.addEventListener('submit', removeMessage);
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageData = load(LOCALSTORAGE_KEY);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
+
+function saveInput() {
+  const saveObj = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(saveObj));
+}
+
+function removeMessage(evt) {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
