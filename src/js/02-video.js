9f84245f08cf3dc574919b0e41b8e930d86b45ee
Andrii Marchuk <andrii.marchuck@gmail.com>
2022-12-25 22:02:35 +0200

    Доробка 2 таску

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,7 @@ import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const saveTime = localStorage.getItem('videoplayer-current-time');
 
 player.on(
   'timeupdate',
@@ -10,8 +11,9 @@ player.on(
     console.log(evt.seconds);
   }, 1000)
 );
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .catch(function (error) {
+
+if (saveTime) {
+  player.setCurrentTime(saveTime).catch(function (error) {
     console.log(error);
   });
+}
